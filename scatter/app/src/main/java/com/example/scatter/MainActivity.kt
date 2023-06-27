9622c5b96fc6468fde8a7b0f4a0f600849bf9b51
Lee <ehgus8621@gmail.com>
2023-06-27 15:39:12 +0900

    dd

--- scatter/app/src/main/java/com/example/scatter/MainActivity.kt
+++ scatter/app/src/main/java/com/example/scatter/MainActivity.kt
@@ -205,21 +205,21 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun sendLocationToServer(latitude: Double, longitude: Double) {
-        val brokerUrl = "tcp://172.30.67.117:1883"
+        val brokerUrl = "tcp://115.21.135.45:1883"
         val clientId = "Phone_GPS"
-        val payload = "disconnected".toByteArray(Charsets.UTF_8)
+//        val payload = "disconnected".toByteArray(Charsets.UTF_8)
 
         try {
             mqttClient = MqttClient(brokerUrl, clientId, MemoryPersistence())
             val mqttConnectOptions = MqttConnectOptions()
             mqttConnectOptions.connectionTimeout = 1000
-            mqttConnectOptions.setWill("location", payload, 1, false)
+//            mqttConnectOptions.setWill("location", payload, 1, false)
             mqttClient.connect(mqttConnectOptions)
         } catch(ex: MqttException){
             ex.printStackTrace()
         }
         val topic = "location"
-        val message = "$latitude : $longitude"
+        val message = "$latitude" + "," + "$longitude"
         val mqttMessage = MqttMessage(message.toByteArray())
         mqttClient.publish(topic, mqttMessage)
     }
