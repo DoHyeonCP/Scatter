4052a015437a0b76a096813e349f181f608cd22f
Lee <ehgus8621@gmail.com>
2023-07-02 04:14:23 +0900

    하

--- scatter/app/src/main/java/com/example/scatter/FirebaseMessagingService.kt
+++ scatter/app/src/main/java/com/example/scatter/FirebaseMessagingService.kt
@@ -31,7 +31,7 @@ class FirebaseMessagingService : FirebaseMessagingService(){
             this,
             0,
             Intent(this, MainActivity::class.java),
-            PendingIntent.FLAG_IMMUTABLE // 뭔가 이상함
+            PendingIntent.FLAG_UPDATE_CURRENT// 뭔가 이상함
         )
 
         val mBuilder = NotificationCompat.Builder(this, "channal_id")
--- scatter/app/src/main/java/com/example/scatter/MainActivity.kt
+++ scatter/app/src/main/java/com/example/scatter/MainActivity.kt
@@ -17,7 +17,10 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -30,7 +33,7 @@ import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var mainbinding: MainActivityBinding
     private lateinit var locationManager: LocationManager
     private lateinit var toolbarbinding: ToolbarBinding
@@ -42,6 +45,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var drawerbutton : Button
     private lateinit var listView: ListView
+    private lateinit var textbody : TextView
+    private lateinit var skApi : SkApi
+    private lateinit var navigationView: NavigationView
     companion object {
         private const val TAG = "MainActivity"
     }
@@ -50,34 +56,28 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         mainbinding = MainActivityBinding.inflate(layoutInflater)
-        toolbarbinding = ToolbarBinding.inflate(layoutInflater)
-        toolbarheadbinding = ToolbarHeadBinding.inflate(layoutInflater)
 
         setContentView(mainbinding.root)
 
 
-
-        toolbar = findViewById(R.id.include)
-        ivMenu = findViewById(R.id.iv_menu)
-        close_menu = toolbarheadbinding.closeMenu
-//        drawerLayout = findViewById(R.layout.drawe)
+//        toolbar = findViewById(R.id.include)
+//        ivMenu = findViewById(R.id.iv_menu)
+        drawerLayout = findViewById(R.id.drawblelayout)
 //        drawerbutton = findViewById(R.id.drawer_button)
+//        button = mainbinding.prediction
+        textbody = mainbinding.textbody
+        navigationView = findViewById(R.id.navigation)
+        skApi = SkApi()
 
-        button = mainbinding.prediction
-
-//        setSupportActionBar(toolbar)
-
-        close_menu.setOnClickListener() {
-            drawerLayout.closeDrawer(Gravity.RIGHT)
-        }
-
-
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu_burger) // 홈버튼 이미지 변경
 
 
-        button.setOnClickListener {
-            val intent = Intent(this@MainActivity, CongetionPrediction::class.java)
-            startActivity(intent)
-        }
+//        button.setOnClickListener {
+//            val intent = Intent(this@MainActivity, CongetionPrediction::class.java)
+//            startActivity(intent)
+//        }
 
 
 //        val congetioninfohead = mainbinding.textHead
@@ -87,16 +87,7 @@ class MainActivity : AppCompatActivity() {
 
         // 디버깅용 코드 3줄 SkApi에 지역을 넣었을 때 return 되도록 해야한다.
 
-        val skApi = SkApi()
-        skApi.jsonPharshing()
-        var u = skApi.update_date
-        var a = skApi.area
-        var c = skApi.congestion_level
-        val congetioninfobody = mainbinding.textBody
-        congetioninfobody.text =
-            "기준시간:      $u \n\n" +
-                    "지역 이름:     $a \n\n" +
-                    "위험도(혼잡도): $c \n\n"
+
 
 
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -107,44 +98,52 @@ class MainActivity : AppCompatActivity() {
             .addOnSuccessListener { token ->
                 Log.d("token", "$token")
             }
+
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            android.R.id.home->{ // 메뉴 버튼
+                drawerLayout.openDrawer(Gravity.LEFT)    // 네비게이션 드로어 열기
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.menu_navigation, menu)       // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId){
-//            R.id.login ->{
-//                val intent = Intent(this@MainActivity, LoginActivity::class.java)
-//                return true
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId){
-//            R.id.login -> {
-//                Log.d("NavigationView", "메뉴 아이템 클릭됨: ${item.itemId}")
-//                val intent = Intent(this@MainActivity, LoginActivity::class.java)
-//                startActivity(intent)
-//            }
-//            R.id.lotteWorld->Log.d("NavigationView", "메뉴 아이템 클릭됨: ${item.itemId}")
-//        }
-//        drawerLayout.closeDrawers() // 기능을 수행하고 네비게이션을 닫아준다.
-//        return false
-//    }
+            R.id.lotteWorld -> {
+                Toast.makeText(this,"불러오는 중",Toast.LENGTH_SHORT).show()
+                skApi = SkApi()
+                skApi.jsonPharshing()
+                var u = skApi.update_date
+                var a = skApi.area
+                var c = skApi.congestion_level
+                val congetioninfobody = textbody
+                congetioninfobody.text =
+                    "기준시간:      $u \n\n" +
+                            "지역 이름:     $a \n\n" +
+                            "위험도(혼잡도): $c \n\n"
+                Toast.makeText(this,"불러왔습니다",Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawers() // 기능을 수행하고 네비게이션을 닫아준다.
+        return true
+    }
 
-//    override fun onBackPressed() {
-//        if(drawerLayout.isDrawerOpen(Gravity.LEFT)){
-//            drawerLayout.closeDrawers()
-//        }else{
-//            super.onBackPressed()
-//        }
-//    }
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(Gravity.LEFT)){
+            drawerLayout.closeDrawers()
+        }else{
+            super.onBackPressed()
+        }
+    }
 
     override fun onResume() {
         super.onResume()
