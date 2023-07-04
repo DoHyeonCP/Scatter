2f6604368f477faddebdac335d473ff77e2df28d
Lee <ehgus8621@gmail.com>
2023-07-04 21:03:46 +0900

    forecast90per

--- scatter/app/src/main/java/com/example/scatter/CongetionPrediction.kt
+++ scatter/app/src/main/java/com/example/scatter/CongetionPrediction.kt
@@ -1,5 +1,6 @@
 package com.example.scatter
 
+import android.content.Intent
 import android.net.http.SslError
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -8,7 +9,10 @@ import android.webkit.SslErrorHandler
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
@@ -24,7 +28,7 @@ class CongetionPrediction : AppCompatActivity() {
     private lateinit var spinner2 : Spinner
     private lateinit var graphview1 : ImageView
     private lateinit var graphview2 : ImageView
-
+    private lateinit var backButton : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Scatter)
         super.onCreate(savedInstanceState)
@@ -32,6 +36,10 @@ class CongetionPrediction : AppCompatActivity() {
         predictionBinding = CongetionPredictionBinding.inflate(layoutInflater)
 
         setContentView(predictionBinding.root)
+
+        backButton = predictionBinding.backButton
+
+
         spinner1 = predictionBinding.spinner1
         graphview1 = predictionBinding.graph1
         val itemArray = arrayOf("지역을 선택해주세요", "롯데월드", "방이동먹자골목", "에비뉴엘월드타워점", "롯데월드몰", "올림픽공원")
@@ -50,36 +58,57 @@ class CongetionPrediction : AppCompatActivity() {
             itemArray
         )
         spinner2.adapter = spinnerAdapter2
-//        spinner1.viewTreeObserver.addOnGlobalLayoutListener {
-//            (spinner.selectedView as TextView).setTextColor(Color.White)
-//            (spinner.selectedView as TextView).setBackgroundResource(R.drawable.spinner_custom)
-//        }
-//        spinner.onItemSelectedListener =
-//            object : AdapterView.OnItemSelectedListener {
-//                override fun onItemSelected(
-//                    parent: AdapterView<*>,
-//                    view: View,
-//                    position: Int,
-//                    id: Long
-//                ) {
-//                    // 선택됬을 경우
-//
-//                }
-//
-//                override fun onNothingSelected(parent: AdapterView<*>) {}
-//            }
 
+
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    val seletedRegion = itemArray[position]
+                    val imageResurceID = getImageResourceId(seletedRegion)
+                    graphview1.setImageResource(imageResurceID)
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {}
+            }
+
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val seletedRegion = itemArray[position]
+                val imageResurceID = getImageResourceId(seletedRegion)
+                graphview2.setImageResource(imageResurceID)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        backButton.setOnClickListener{
+            val intent = Intent(this@CongetionPrediction, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-}
 
-// webView = predictionBinding.predictionGraph
-//        webView.settings.javaScriptEnabled = true
-//        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-//
-//        webView.webViewClient = object : WebViewClient(){
-//            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?){
-//                handler?.proceed()
-//            }
-//        }
-//
-//        webView.loadUrl("https://www.naver.com")
+
+    private fun getImageResourceId(region: String): Int {
+        return when (region) {
+            "롯데월드" -> R.drawable.jamsillotteworld
+            "방이동먹자골목" -> R.drawable.bangi
+            "에비뉴엘월드타워점" -> R.drawable.avinual
+            "롯데월드몰" -> R.drawable.lotteworldmall
+            "올림픽공원" -> R.drawable.oplimpicpark
+            else -> R.drawable.whitescreen // 기본 이미지 리소스 ID
+        }
+    }
+
+
+}
