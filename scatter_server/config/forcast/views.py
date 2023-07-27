bbb9103086ff7824771c14fe37308d021017b176
Lee <ehgus8621@gmail.com>
2023-07-27 16:39:59 +0900

    json계층분리,AI_nodango에서생성된파일db에저장하도록하기

--- scatter_server/config/forcast/views.py
+++ scatter_server/config/forcast/views.py
@@ -11,12 +11,9 @@ def export_images_as_json(request):
     image_folder = 'AI_nodjango/images' # 템플릿 디렉토리 경로
 
     image_files = os.listdir(image_folder)
-    image_data = []
+    image_data = {}
     for image_file in image_files:
         image_path = os.path.join(image_file, image_folder)
-        image_data.append({
-            'name': image_file,
-            'path': image_path,
-        })
+        image_data[image_file] = {'path': image_path}
         
     return JsonResponse(image_data, safe = False)
