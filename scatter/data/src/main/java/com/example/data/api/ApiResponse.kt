63f0773442f2c99f9c0974362f6713bc772e5855
Lee <ehgus8621@gmail.com>
2023-08-18 02:19:11 +0900

    모듈생성

---
+++ scatter/data/src/main/java/com/example/data/api/ApiResponse.kt
@@ -0,0 +1,13 @@
+package com.example.data.api
+
+import com.example.data.model.Hotspot
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse(
+    @SerializedName("롯데월드") val 롯데월드: Hotspot,
+    @SerializedName("방이동먹자골목") val 방이동먹자골목: Hotspot,
+    @SerializedName("에비뉴엘월드타워점") val 에비뉴엘월드타워점: Hotspot,
+    @SerializedName("롯데월드몰") val 롯데월드몰: Hotspot,
+    @SerializedName("올림픽공원") val 올림픽공원: Hotspot,
+
+    )
---
+++ scatter/data/src/main/java/com/example/data/firebase/FirebaseMessagingService.kt
@@ -0,0 +1,60 @@
+package com.example.data.firebase
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+
+import androidx.core.app.NotificationCompat
+import androidx.core.graphics.red
+import com.example.scatter.activity.MainActivity
+import com.example.scatter.R
+
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessagingService : FirebaseMessagingService(){
+    private val TAG = "FirebasemsgService"
+    private var msg: String? = null
+    private var title: String? = null
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.e(TAG, "onMessageRecived")
+        title = remoteMessage.notification?.title
+        msg = remoteMessage.notification?.body
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT// 뭔가 이상함
+        )
+
+        val mBuilder = NotificationCompat.Builder(this, "channal_id")
+            .setSmallIcon(R.drawable.congestion_emo)
+            .setContentTitle(title)
+            .setContentText(msg)
+            .setAutoCancel(true)
+
+
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setVibrate(longArrayOf(1,1000))
+            .extend(WearableExtender)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        notificationManager.notify(0, mBuilder.build())
+
+        mBuilder.setContentIntent(contentIntent)
+    }
+
+    val WearableExtender = NotificationCompat.WearableExtender()
+        .setContentAction(R.drawable.congestion_emo.red)
+
+}
