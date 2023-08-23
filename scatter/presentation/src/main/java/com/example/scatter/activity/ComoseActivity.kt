3e4d2803ed6179a5dd1ebd41d69dffefe6b0b8e0
Lee <ehgus8621@gmail.com>
2023-08-23 17:56:38 +0900

    툴바생성

--- scatter/presentation/src/main/java/com/example/scatter/activity/ComoseActivity.kt
+++ scatter/presentation/src/main/java/com/example/scatter/activity/ComoseActivity.kt
@@ -1,73 +1,206 @@
 package com.example.scatter.activity
 
+import com.example.scatter.R
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationRailDefaults.ContainerColor
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.scatter.ui.theme.NavigationDrawerComposeTheme
 import kotlinx.coroutines.launch
 
-class ComposeActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class ComoseActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContent {
-            NavigationDrawerComposeTheme {
-                val scaffoldState = rememberScaffoldState()
-                val scope = rememberCoroutineScope()
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    topBar = {
-                        AppBar(
-                            onNavigationIconClick = {
-                                scope.launch {
-                                    scaffoldState.drawerState.open()
-                                }
-                            }
-                        )
-                    },
-                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
-                    drawerContent = {
-                        DrawerHeader()
-                        DrawerBody(
-                            items = listOf(
-                                MenuItem(
-                                    id = "home",
-                                    title = "Home",
-                                    contentDescription = "Go to home screen",
-                                    icon = Icons.Default.Home
-                                ),
-                                MenuItem(
-                                    id = "settings",
-                                    title = "Settings",
-                                    contentDescription = "Go to settings screen",
-                                    icon = Icons.Default.Settings
-                                ),
-                                MenuItem(
-                                    id = "help",
-                                    title = "Help",
-                                    contentDescription = "Get help",
-                                    icon = Icons.Default.Info
-                                ),
-                            ),
-                            onItemClick = {
-                                println("Clicked on ${it.title}")
-                            }
-                        )
-                    }
-                ) {
+        setContent{
+            Surface(
+                modifier = Modifier.fillMaxSize(),
 
-                }
+            ) {
+                MyScaffoldLayout()
             }
         }
+    }
+}
+
+//@Composable
+//fun Toolbar(){
+//    Scaffold(
+//        topBar = {
+//            TopAppBar(
+//                title = {Text(text = toolbar},
+//
+//            )
+//        }
+//    )
+//}
+data class Message(val author: String, val body:String)
+
+@Composable
+fun Main(msg: Message){
+    Column{
+        Image(
+            painter = painterResource(R.drawable.jamsil_map),
+            contentDescription = "contact profile picture",
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class) // 이걸 안붙이는 방법 찾아야함
+@Composable
+fun MyTopAppBar(onNaviagionIconClick: () -> Unit){
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = {
+                onNaviagionIconClick()
+            }){
+                Icon(
+                    imageVector = Icons.Outlined.Menu,
+                    contentDescription = "navigation"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun MyNavigationDrawer() {
+
+}
+
+@Composable
+fun MySnackbar() {
+
+}
+
+@Composable
+fun MyFAB() {
+
+}
+
+@Composable
+fun MyBottomBar() {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyScaffoldLayout() {
+//    val scaffoldState = rememberScaffoldState() M3 존재하지 않음 https://developer.android.com/jetpack/compose/designsystems/material2-material3?hl=ko
+    val snackbarHostState = remember {SnackbarHostState()}
+    val coroutineScope = rememberCoroutineScope()
+    val contextForToast = LocalContext.current.applicationContext
+
+//    scaffoldState = scaffoldState 이거 왜 안됨.
+    Scaffold(
+        topBar = {
+            MyTopAppBar {
+
+            }
+        }
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = it), // use "it" parameter
+        ) {
+            // we will add a button to display the snackbar here
+        }
+    }
+}
+
+sealed class MenuItems(
+    val 롯데월드: String,
+    val 방이동_먹자골목: String,
+    val 에비뉴엘월드타워점: String,
+    val 롯데월드몰: String,
+    val 올림픽공원: String,
+)
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun toolbar(){
+//    val coroutineScope = rememberCoroutineScope()
+//
+//    Scaffold(
+//        topBar ={
+//            TopAppBar(
+//                title = { },
+//                navigationIcon = {
+//                    IconButton(onClick = {}) {
+//                        Icon(
+//                            imageVector = Icons.Filled.Menu,
+//                            contentDescription = "Menu"
+//                        )
+//                    }
+//                },
+//            )
+//        },
+//        content = { PaddingValues ->
+//            Column {
+//
+//                Text("Item Name")
+//                Text("Item Description")
+//            }
+//        }
+//    )
+//}
+
+
+@Preview
+@Composable
+fun ToolbarSamplePreview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+
+        ) {
+        MyScaffoldLayout()
     }
 }
--- scatter/presentation/src/main/java/com/example/scatter/activity/Toolbar.kt
+++ scatter/presentation/src/main/java/com/example/scatter/activity/Toolbar.kt
@@ -4,26 +4,35 @@
 //import androidx.compose.foundation.layout.fillMaxSize
 //import androidx.compose.material.icons.Icons
 //import androidx.compose.material.icons.filled.Menu
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.IconButton
-//import androidx.compose.material3.Text
-//import androidx.compose.material3.TopAppBar
-//import androidx.compose.runtime.Composable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import android.media.Image
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
 //import androidx.compose.ui.Modifier
 //import androidx.compose.ui.graphics.Color
 //
 //// sealed class로 정의하면, 다이나믹 데이터를 추가할 수 있지만,
 //// enum class로 들어오변, iterate을 하기 편함
 //
-//enum class DrawerMenus(
-//    val 롯데월드: String,
-//    val 방이동_먹자골목: String,
-//    val 에비뉴엘월드타워점: String,
-//    val 롯데월드몰: String,
-//    val 올림픽공원: String,
-//){
-//
-//}
+enum class Drawer(
+    val burger: Image,
+    val logo: Image
+)
+sealed class MenuItems(
+    val 롯데월드: String,
+    val 방이동_먹자골목: String,
+    val 에비뉴엘월드타워점: String,
+    val 롯데월드몰: String,
+    val 올림픽공원: String,
+){
+
+}
 //
 //// 메뉴들을 클릭했을 때 보여줄 UI 정의
 //@Composable
@@ -34,20 +43,26 @@
 //}
 //
 //// AppBar Composable, 페이지안에 추가해도되지만, 공통으로 단순한 형태를 사용할 경우
-//// AppBar의 메뉴아이콘을 클릭하면, drawer 열리게 하다록 위함.
-//@Composable
-//fun TopBar(
-//    onMenuClick: () -> Unit
-//){
-//    TopAppBar(
-//        title = {Text("My App")},
+// AppBar의 메뉴아이콘을 클릭하면, drawer 열리게 하다록 위함.
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun toolbar(
+){
+    TopAppBar(
+        title = {Text("My App")},
 //        navigationIcon = {
 //            IconButton(onClick = onMenuClick) {
 //
 //            }
-//        }
-//    )
-//}
+//        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToolbarSamplePreview() {
+      toolbar()
+}
 //
 //val scaffoldState = rememberScaffoldState()
 //val scope = remeberCoroutineScope()
