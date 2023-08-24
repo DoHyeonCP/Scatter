2c91362a728e0a9b75dbc12f575b62de24751519
Lee <ehgus8621@gmail.com>
2023-08-24 21:59:16 +0900

    success_scaffold_topbar

--- scatter/presentation/src/main/java/com/example/scatter/activity/Scaffold.kt
+++ scatter/presentation/src/main/java/com/example/scatter/activity/Scaffold.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -24,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -55,27 +60,42 @@ fun MyScaffoldLayout(content: @Composable (PaddingValues) -> Unit) {
 }
 
 @Composable
-fun MyTopAppBar(onNaviagionIconClick: () -> Unit){
+fun MyTopAppBar(onNaviagionIconClick: () -> Unit) {
     TopAppBar(
-        title = {
-                Image(
-                    painter = painterResource(id = R.drawable.congestionlogo),
-                    contentDescription = "center_image",
-              )
-        },
-        navigationIcon = {
-            IconButton(onClick = {
-                onNaviagionIconClick()
-            }){
+        backgroundColor = Color(0xFF10316B),
+        contentColor = Color.White,
+        elevation = 10.dp
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // 네비게이션 아이콘
+            IconButton(onClick = onNaviagionIconClick) {
                 Icon(
                     imageVector = Icons.Outlined.Menu,
-                    contentDescription = "navigation"
+                    contentDescription = "navigation",
+                    tint = Color.White, // 안하면 배경색이 같이 바뀜
+                    modifier = Modifier.size(60.dp) // 아이콘 크기를 36dp로 키우기
                 )
             }
-        },
-        backgroundColor = Color.Black
-    )
+
+            // 가운데 로고
+            Image(
+                painter = painterResource(id = R.drawable.congestionlogo),
+                contentDescription = "center_image",
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+
+            // 아이콘과 동일한 크기의 빈 공간을 만들어 로고를 중앙에 유지합니다.
+            Spacer(modifier = Modifier.size(60.dp))
+        }
+    }
 }
+
+
+
+
 
 @Composable
 fun MyNavigationDrawer(
@@ -104,5 +124,4 @@ fun MyNavigationDrawer(
         }
     }
 }
-
 
